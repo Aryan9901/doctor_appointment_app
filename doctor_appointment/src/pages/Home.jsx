55280bdfefe83bce4739cfa55ebe3b9a6aedc155
Aryan9901 <911aaryan@gmail.com>
2024-03-23 22:31:10 +0530

    home page initiated

--- doctor_appointment/src/pages/Home.jsx
+++ doctor_appointment/src/pages/Home.jsx
@@ -1,5 +1,13 @@
 const Home = () => {
-	return <div>hello</div>;
+	return (
+		<main className="mt-[3.4%] w-full min-h-screen">
+			<section
+				className="hero-section w-full h-[130vh] bg-[url(/heroimg.png)] bg-cover  mx-auto"
+				style={{ borderRadius: "2rem" }}
+				// style={{ borderTopLeftRadius: "2rem", borderTopRightRadius: "2rem" }}
+			></section>
+		</main>
+	);
 };
 
 export default Home;
