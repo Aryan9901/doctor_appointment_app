db0b1a1ea4cfcb1f280da05c2a5337710f7ee2e1
Aryan9901 <911aaryan@gmail.com>
2024-03-23 23:47:39 +0530

    hero section completed

--- doctor_appointment/src/pages/Home.jsx
+++ doctor_appointment/src/pages/Home.jsx
@@ -3,9 +3,9 @@ import { Button } from "../components/ui/button";
 const Home = () => {
 	return (
 		<main className="mt-[3.4%] w-full min-h-screen">
-			<section className="hero-section pt-2 w-full h-[130vh] bg-[url(/heroimg.png)] bg-cover  mx-auto" style={{ borderRadius: "2rem" }}>
-				<div className="h-3/4 w-1/2 mt-11">
-					<div className="h-full w-4/5 ml-auto flex items-start justify-center gap-3 flex-col gap-9">
+			<section className="hero-section pt-2 w-full h-[150vh] bg-[url(/heroimg.png)] bg-cover  mx-auto" style={{ borderRadius: "2rem" }}>
+				<div className="h-[72%] w-1/2 mt-11">
+					<div className="h-full w-4/5 ml-auto flex items-start justify-center flex-col gap-5">
 						<div className="flex items-center justify-start gap-2 px-9 py-2 rounded-3xl bg-[#34353641] text-xl font-semibold text-white">
 							<p>Professional</p>
 							<div className="w-10 h-[1.2px] bg-[#04ce78]"></div>
@@ -26,13 +26,50 @@ const Home = () => {
 							A brief statement outlining the purpose and mission of the clinic. This can include the commitment to patient care,
 							community health.
 						</article>
-						<div>
-							<Button className="px-8 py-5 rounded-3xl bg-gradient-green text-black">See All Services</Button>
+						<div className="flex items-start justify-start gap-5">
+							<Button variant="secondary" className="px-8 py-5 rounded-3xl bg-gradient-green text-black">
+								See All Services
+							</Button>
 							<Button variant="secondary" className="px-8 py-5 rounded-3xl bg-[#EEF2FD] text-[#252a3a]">
 								Contact Us
 							</Button>
 						</div>
 					</div>
+				</div>
+				<div className="px-6 appointment-form mr-6 w-[87%] rounded-[3rem] h-44 box-shadow bg-white ml-auto flex justify-between items-center">
+					<h3 className="text-4xl text-[#000D44] font-bold pr-3 border-r-2 w-[25%] border-[#04ce78]">Book An Appointment</h3>
+					<form className="flex flex-wrap items-center justify-around w-[75%] gap-4">
+						<input
+							type="text"
+							className="py-3 px-8 outline-none focus:shadow-md border-[#000] rounded-3xl bg-slate-200 text-sm"
+							placeholder="Your Name"
+						/>
+						<input
+							type="text"
+							className="py-3 px-8 outline-none focus:shadow-md border-[#000] rounded-3xl bg-slate-200 text-sm"
+							placeholder="Your Name"
+						/>
+						<input
+							type="text"
+							className="py-3 px-8 outline-none focus:shadow-md border-[#000] rounded-3xl bg-slate-200 text-sm"
+							placeholder="Your Name"
+						/>
+						<input
+							type="text"
+							className="py-3 px-8 outline-none focus:shadow-md border-[#000] rounded-3xl bg-slate-200 text-sm"
+							placeholder="Your Name"
+						/>
+						<input
+							type="text"
+							className="py-3 px-8 outline-none focus:shadow-md border-[#000] rounded-3xl bg-slate-200 text-sm"
+							placeholder="Your Name"
+						/>
+						<input
+							type="text"
+							className="py-3 px-8 outline-none focus:shadow-md border-[#000] rounded-3xl bg-slate-200 text-sm"
+							placeholder="Your Name"
+						/>
+					</form>
 				</div>
 			</section>
 		</main>
