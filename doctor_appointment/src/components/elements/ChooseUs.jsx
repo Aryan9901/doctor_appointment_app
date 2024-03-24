89d74f09045dc8e99bf820253885279cb2dbdc13
Aryan9901 <911aaryan@gmail.com>
2024-03-24 15:23:09 +0530

    updated why choose us page

--- doctor_appointment/src/components/elements/ChooseUs.jsx
+++ doctor_appointment/src/components/elements/ChooseUs.jsx
@@ -1,5 +1,16 @@
 const ChooseUs = () => {
-	return <section className="min-h-screen bg-slate-400"></section>;
+	return (
+		<section className="min-h-screen py-20 px-10 relative">
+			<img src="/dental_5.png" className="absolute right-6 top-12 animate-bounce duration-1000 w-80  duration-1000" alt="dental teeth" />
+			<h3 className="flex gap-5 text-[#05CE79] text-2xl items-center justify-center">
+				<img src="/title_icon.svg" alt="title icon" />
+				Why Choose Us
+			</h3>
+			<h2 className="text-4xl my-3 text-center font-semibold text-[#000d44] w-[45%] my-4 mx-auto flex items-center justify-between">
+				We’re Passionate, Dedicated And Friendly...
+			</h2>
+		</section>
+	);
 };
 
 export default ChooseUs;
