cdd5973121f6df379b11a610658609be8189191a
Aryan9901 <911aaryan@gmail.com>
2024-03-24 17:39:30 +0530

    card and tab component downloaded

--- doctor_appointment/src/components/elements/ChooseUs.jsx
+++ doctor_appointment/src/components/elements/ChooseUs.jsx
@@ -1,6 +1,8 @@
+import TabsContainer from "./TabsContainer";
+
 const ChooseUs = () => {
 	return (
-		<section className="min-h-screen py-20 px-10 relative">
+		<section className="min-h-screen py-20 px-10 relative bg-[url(pattern_shape_3.png)] bg-cover">
 			<img src="/dental_5.png" className="absolute right-6 top-12 animate-bounce duration-1000 w-80  duration-1000" alt="dental teeth" />
 			<h3 className="flex gap-5 text-[#05CE79] text-2xl items-center justify-center">
 				<img src="/title_icon.svg" alt="title icon" />
@@ -9,6 +11,7 @@ const ChooseUs = () => {
 			<h2 className="text-4xl my-3 text-center font-semibold text-[#000d44] w-[45%] my-4 mx-auto flex items-center justify-between">
 				We’re Passionate, Dedicated And Friendly...
 			</h2>
+			<TabsContainer />
 		</section>
 	);
 };
