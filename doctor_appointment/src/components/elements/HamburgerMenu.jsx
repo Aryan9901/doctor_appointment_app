b89425778e32b875f2e535649b56eaa39e14af10
Aryan9901 <911aaryan@gmail.com>
2024-03-28 20:52:32 +0530

    navbar is responsive now

--- doctor_appointment/src/components/elements/HamburgerMenu.jsx
+++ doctor_appointment/src/components/elements/HamburgerMenu.jsx
@@ -1,11 +1,11 @@
-import { SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { SheetContent, SheetDescription, SheetHeader } from "@/components/ui/sheet";
 import { Button } from "../ui/button";
 
 const HamburgerMenu = () => {
 	return (
-		<SheetContent className="w-[30%]">
-			<SheetHeader>
-				<SheetDescription className="flex items-center justify-center mt-8">
+		<SheetContent className="w-[49%] box-border p-0">
+			<SheetHeader className="w-full py-12 px-5">
+				<SheetDescription className="flex w-full items-center justify-center mt-8">
 					<nav className="flex flex-col items-center justify-evenly text-black w-full font-bold ">
 						<Button variant="outline" className="mb-9 px-6 w-full py-7 hover:bg-slate-400 bg-slate-800 text-white">
 							<a href="/" className="text-2xl font-bold">
--- doctor_appointment/src/components/elements/Navbar.jsx
+++ doctor_appointment/src/components/elements/Navbar.jsx
@@ -10,53 +10,65 @@ import HamburgerMenu from "./HamburgerMenu";
 
 const Navbar = () => {
 	const [isSmallerScreen, setIsSmallerScreen] = useState(false);
+	const [isScrolled, setIsScrolled] = useState(false);
 
 	useEffect(() => {
 		const handleResize = () => {
 			setIsSmallerScreen(window.innerWidth <= 640);
 		};
 
+		const handleScroll = () => {
+			// Check if the scroll position is greater than 50 pixels
+			setIsScrolled(window.scrollY > 50);
+		};
+
 		// Initial call to handleResize
 		handleResize();
+		handleScroll();
 
-		// Event listener for window resize
+		// Event listeners for window resize and scroll
 		window.addEventListener("resize", handleResize);
+		window.addEventListener("scroll", handleScroll);
 
-		// Cleanup function to remove the event listener when component unmounts
+		// Cleanup function to remove the event listeners when component unmounts
 		return () => {
 			window.removeEventListener("resize", handleResize);
+			window.removeEventListener("scroll", handleScroll);
 		};
 	}, []);
 
 	return (
 		<header
-			className={`w-[90%] mx-auto max-w-screen-2xl h-28 shadow-md absolute ${
-				isSmallerScreen ? "top-[-.4%]" : "top-[-.65%]"
-			} left-1/2 -translate-x-1/2`}
+			className={`w-[90%] mx-auto max-w-screen-2xl ${isScrolled ? "absolute" : "fixed"} h-28 shadow-md top-0 left-1/2 -translate-x-1/2`}
 			style={{ borderBottomRightRadius: "9px" }}
 		>
-			<div className="w-[20%] px-3 py-5 bg-black clip-custom absolute top-0 h-full left-0" style={{ borderBottomLeftRadius: "9px" }}>
-				<img src="/logo.svg" className="w-[80%] h-full" alt="logo" />
+			<div
+				className={`${isSmallerScreen ? "w-[35%]" : "w-[20%]"} px-3 py-5 bg-black clip-custom absolute top-0 h-full left-0`}
+				style={{ borderBottomLeftRadius: "9px" }}
+			>
+				<img src="/logo.svg" className={`${isSmallerScreen ? "w-[92%]" : "w-[80%]"} h-full`} alt="logo" />
 			</div>
 			<div className="w-[100%] bg-white pl-[20%] text-white h-full" style={{ borderBottomRightRadius: "9px", borderBottomLeftRadius: "9px" }}>
 				<div className="header-top w-full  px-3 bg-[#04ce78] h-[39%] flex justify-between items-center">
-					<div className="icons flex justify-start items-center gap-2">
-						<div className="text-sm flex items-center gap-1 text-[.7rem]">
-							<span className="h-6 w-6 rounded-full flex items-center justify-center border">
-								<FaPhoneAlt className="text-[.6rem]" />
-							</span>
-							Phone: 91-7732636464
-						</div>
-						<div className="flex justify-start items-center gap-2">
+					{!isSmallerScreen && (
+						<div className="icons flex justify-start items-center gap-2">
 							<div className="text-sm flex items-center gap-1 text-[.7rem]">
 								<span className="h-6 w-6 rounded-full flex items-center justify-center border">
-									<IoMail className="text-[.8rem]" />
+									<FaPhoneAlt className="text-[.6rem]" />
 								</span>
-								Email: info@gmail.com
+								Phone: 91-7732636464
+							</div>
+							<div className="flex justify-start items-center gap-2">
+								<div className="text-sm flex items-center gap-1 text-[.7rem]">
+									<span className="h-6 w-6 rounded-full flex items-center justify-center border">
+										<IoMail className="text-[.8rem]" />
+									</span>
+									Email: info@gmail.com
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className="socials flex items-center justify-end gap-4 text-[.9rem]">
+					)}
+					<div className={`socials flex items-center ${isSmallerScreen ? "ml-auto" : ""} justify-end gap-4 text-[.9rem]`}>
 						<h4>Follow us on:</h4>
 						<a href="/">
 							<FaFacebook />
@@ -104,7 +116,7 @@ const Navbar = () => {
 							</Sheet>
 						)}
 						{isSmallerScreen && (
-							<Sheet>
+							<Sheet className="w-100">
 								<SheetTrigger>
 									<div className="h-10 w-10 border rounded-full flex justify-center items-center cursor-pointer hover:bg-[#04ce78] duration-500">
 										<RxHamburgerMenu className="text-red-600 text-xl font-bold" />
